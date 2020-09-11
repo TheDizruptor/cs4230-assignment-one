cc9f933b7992720017e38260e2f75f1cdc15fa91
Justin <jstnwrds55@gmail.com>
2020-09-11 17:18:13 -0600

    fixed address validation logic

--- src/main/java/edu/dizruptor/MyFirstServlet.java
+++ src/main/java/edu/dizruptor/MyFirstServlet.java
@@ -60,12 +60,15 @@ public class MyFirstServlet extends HttpServlet
 		String postal2 = req.getParameter("postal2");
 		String country2 = req.getParameter("country2");
 		// personal info validation
-		if (firstName == null || lastName == null || phoneNumber == null) {
+		if (firstName == null || lastName == null || phoneNumber == null ||
+			firstName.equals("") || lastName.equals("") || phoneNumber.equals("")) {
 			error = "Personal Info Can't Be Empty";	// set error message
 		}
 		// address 1 validation
 		else if (type1 == null || address1 == null || city1 == null ||
-		state1 == null || postal1 == null || country1 == null) {
+				state1 == null || postal1 == null || country1 == null ||
+				type1.equals("") || address1.equals("") || city1.equals("") ||
+				state1.equals("") || postal1.equals("") || country1.equals("")) {
 			error = "Address 1 Can't Be Empty";	// set error message
 		}
 		// validated
@@ -76,8 +79,9 @@ public class MyFirstServlet extends HttpServlet
 			// store first address
 			tempAddresses.add(new Address(type1, address1, city1, state1, postal1, country1));
 			// check for second address
-			if (!(type2 == null || address2 == null || city2 == null ||
-					state2 == null || postal2 == null || country2 == null)) {
+			if (!(type2 == null || address2 == null || city2 == null || state2 == null ||
+				postal2 == null || country2 == null || type2.equals("") || address2.equals("") ||
+				city2.equals("") || state2.equals("") || postal2.equals("") || country2.equals(""))) {
 				// add second address
 				tempAddresses.add(new Address(type2, address2, city2, state2, postal2, country2));
 			}
